bd2cef557b47e3b8741ff3eaf5ef9cf04e640973
Florian-Audouard <nairolf.audouard@gmail.com>
2023-06-26 18:48:51 +1100

    test

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,5 +1,5 @@
-fetch("/getDatabase")
-	.then((res) => res.json())
-	.then((data) => {
-		document.querySelector("#testDatabase").innerText = data[0][1];
-	});
+// fetch("/getDatabase")
+// 	.then((res) => res.json())
+// 	.then((data) => {
+// 		document.querySelector("#testDatabase").innerText = data[0][1];
+// 	});
